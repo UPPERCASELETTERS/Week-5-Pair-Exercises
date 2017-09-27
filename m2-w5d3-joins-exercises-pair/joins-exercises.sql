2db9eedcfa47a3136589fd372efc0b46ea041fe2
Christopher Meigs <cjmeigs017@gmail.com>
2017-09-27 14:00:51 -0400

    W5 D3 completed pair exercises

--- m2-w5d3-joins-exercises-pair/joins-exercises.sql
+++ m2-w5d3-joins-exercises-pair/joins-exercises.sql
@@ -3,30 +3,56 @@
 
 -- 1. The city name, country name, and city population of all cities in Europe with population greater than 1 million
 -- (36 rows)
-
+SELECT cty.name, ctry.name, cty.population FROM country ctry
+JOIN city cty ON cty.countrycode = ctry.code
+WHERE continent = 'Europe' AND cty.population > 1000000;
 -- 2. The city name, country name, and city population of all cities in countries where French is an official language and the city population is greater than 1 million
 -- (2 rows)
+SELECT cty.name, ctry.name, cty.population FROM country ctry 
+JOIN city cty ON cty.countrycode = ctry.code
+JOIN countrylanguage cl ON cl.countrycode = cty.countrycode
+WHERE language = 'French' AND isofficial = true AND cty.population > 1000000;
 
 -- 3. The name of the countries and continents where the language Javanese is spoken
 -- (1 row)
-
+SELECT ctry.name, ctry.continent FROM country ctry
+JOIN countrylanguage cl ON cl.countrycode = ctry.code
+WHERE language = 'Javanese';
 -- 4. The names of all of the countries in Africa that speak French as an official language
 -- (5 row)
-
+SELECT ctry.name FROM country ctry
+JOIN countrylanguage cl ON cl.countrycode = ctry.code
+WHERE language = 'French' AND isofficial = true AND continent = 'Africa';
 -- 5. The average city population of cities in Europe
 -- (average city population in Europe: 287,684)
-
+SELECT AVG(cty.population) FROM country ctry
+JOIN city cty ON cty.countrycode = ctry.code
+WHERE continent = 'Europe'; 
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
-
+SELECT AVG(cty.population) FROM country ctry
+JOIN city cty ON cty.countrycode = ctry.code
+WHERE continent = 'Asia';
 -- 7. The number of cities in countries where English is an official language
 -- (number of cities where English is official language: 523)
-
+SELECT COUNT(cty.name) FROM country ctry
+JOIN city cty ON cty.countrycode = ctry.code
+JOIN countrylanguage cl ON cl.countrycode = cty.countrycode
+WHERE language = 'English' AND isofficial = true;
 -- 8. The average population of cities in countries where the official language is English
 -- (average population of cities where English is official language: 285,809)
-
+SELECT AVG(cty.population) FROM country ctry
+JOIN city cty ON cty.countrycode = ctry.code
+JOIN countrylanguage cl ON cl.countrycode = cty.countrycode
+WHERE language = 'English' AND isofficial = true;
 -- 9. The names of all of the continents and the population of the continent’s largest city
 -- (6 rows, largest population for North America: 8,591,309)
-
+SELECT continent, MAX(cty.population) FROM country ctry
+JOIN city cty ON cty.countrycode = ctry.code
+GROUP BY continent;
 -- 10. The names of all of the cities in South America that have a population of more than 1 million people and the official language of each city’s country
 -- (29 rows)
+SELECT cl.language, cty.name FROM country ctry
+JOIN city cty ON cty.countrycode = ctry.code
+JOIN countrylanguage cl ON cl.countrycode = cty.countrycode
+WHERE cty.population > 1000000 AND continent = 'South America' AND isofficial = true; 
